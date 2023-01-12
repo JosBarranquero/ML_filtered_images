Added Sobel filter and fixed underflow/overflow error

scripts_python/feature_engineering_test.py
@@ -13,17 +13,17 @@ from sklearn.metrics import mean_squared_error
 # Pickle filenames and options to save and load
 original_pkl = 'original-eng.pkl'
 filtered_pkl = 'filtered-eng.pkl'
-save_to_pkl = True
-load_fr_pkl = True
+save_to_pkl = False
+load_fr_pkl = False
 
 # Directories
-original_dir = '../originales/'
-filtered_dir = '../filtradas/'
-predict_dir = '../prediccion/'
+original_dir = './originales/'
+filtered_dir = './filtradas/'
+predict_dir = './prediccion/'
 
 # File extensions
 img_ext = '.bmp'
-filter_ext = '-low' + img_ext
+filter_ext = '-high' + img_ext
 pred_ext = 'pred-{0}'
 
 # Image characteristics
@@ -136,6 +136,13 @@ for i in range (0, len(y_pred)):
     # Converting from a (height*width) vector back to a heightXwidth matrix
     # pred_img = np.reshape(y_pred[i], newshape=(height, width))
     pred_img = y_pred[i]
+    
+    # Checking for underflow and overflow
+    if (pred_img < 0):
+        pred_img = np.array([0])
+    elif (pred_img > 255):
+        pred_img = np.array([255])
+
     pred_img = pred_img.astype(np.uint8)
     
     # imagen_real = y_test.loc[i+75,:]

scripts_python/filters.py
@@ -2,8 +2,8 @@ import cv2 as cv
 import numpy as np
 import math
 
-def low_pass_filter(in_file: str, out_file: str, size: int = 3):
-    """This function applies a low pass filter to a grayscale image. The resulting image is then save to disk"""
+def low_pass_filter(in_file: str, out_file: str, size: int = 3, type: int = 1):
+    """This function applies a low pass filter to a grayscale image. The resulting image is then saved to disk"""
     # Reading the input image
     original = cv.imread(in_file, cv.IMREAD_UNCHANGED)
 
@@ -14,10 +14,14 @@ def low_pass_filter(in_file: str, out_file: str, size: int = 3):
         raise RuntimeError('Image \'{0}\' is in color!'.format(in_file))
 
     # Creating the matrix filter (sizeXsize)
-    filter = 1/size**2 * np.ones((size, size))     # Type 1 filter
-    # filter = np.array([[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125], [0.0625, 0.125, 0.0625]])   # Type 2 filter
-    # filter = np.zeros((size, size))     # Type 3 filter
-    # filter[math.ceil(size/2), math.ceil(size/2)] = 1.0
+    filter = np.ones((size, size))
+    if (type == 1):     # Type 1 filter
+        filter = 1/size**2 * filter
+    elif (type == 2):   # Type 2 filter
+        filter = np.array([[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125], [0.0625, 0.125, 0.0625]])
+    elif (type == 3):   # Type 3 filter
+        filter = np.zeros((size, size))
+        filter[math.ceil(size/2), math.ceil(size/2)] = 1.0
 
     # Applying the filter
     # Second parameter = -1 : keeps the same colordepth
@@ -27,7 +31,7 @@ def low_pass_filter(in_file: str, out_file: str, size: int = 3):
     cv.imwrite(out_file, result)
 
 def high_pass_filter(in_file: str, out_file: str, size: int = 3):
-    """This function applies a high pass filter to a grayscale image. The resulting image is then save to disk"""
+    """This function applies a high pass filter to a grayscale image. The resulting image is then saved to disk"""
     # Reading the input image
     original = cv.imread(in_file, cv.IMREAD_UNCHANGED)
 
@@ -49,7 +53,7 @@ def high_pass_filter(in_file: str, out_file: str, size: int = 3):
     cv.imwrite(out_file, result)
 
 def median_filter(in_file: str, out_file: str, size: int = 3):
-    """This function applies a median filter to a grayscale image. The resulting image is then save to disk"""
+    """This function applies a median filter to a grayscale image. The resulting image is then saved to disk"""
     # Reading the input image
     original = cv.imread(in_file, cv.IMREAD_UNCHANGED)
     
@@ -64,6 +68,41 @@ def median_filter(in_file: str, out_file: str, size: int = 3):
 
     # Save it to disk
     cv.imwrite(out_file, result)
+
+def hsobel_filter(in_file: str, out_file: str, size: int = 3):
+    """This function applies a horizontal Sobel filter to a grayscale image. The resulting image is then saved to sisk"""
+    # Reading the input image
+    original = cv.imread(in_file, cv.IMREAD_UNCHANGED)
+
+    if original is None:    # If image wasn't read, then the file doesn't exist
+        raise FileNotFoundError('Image \'{0}\' not found'.format(in_file))
+
+    if original.ndim == 3:   # If it's a color image, raise an error
+        raise RuntimeError('Image \'{0}\' is in color!'.format(in_file))
+
+    # Applying horizontal Sobel filter (sizeXsize)
+    result = cv.Sobel(original, -1, 1, 0, ksize=size)
+
+    # Save it to disk
+    cv.imwrite(out_file, result)
+
+def vsobel_filter(in_file: str, out_file: str, size: int = 3):
+    """This function applies a vertical Sobel filter to a grayscale image. The resulting image is then saved to sisk"""
+    # Reading the input image
+    original = cv.imread(in_file, cv.IMREAD_UNCHANGED)
+
+    if original is None:    # If image wasn't read, then the file doesn't exist
+        raise FileNotFoundError('Image \'{0}\' not found'.format(in_file))
+
+    if original.ndim == 3:   # If it's a color image, raise an error
+        raise RuntimeError('Image \'{0}\' is in color!'.format(in_file))
+
+    # Applying vertical Sobel filter (sizeXsize)
+    result = cv.Sobel(original, -1, 0, 1, ksize=size)
+
+    # Save it to disk
+    cv.imwrite(out_file, result)
+
 
 # Show a message if the script is run by itself
 if __name__ == '__main__':